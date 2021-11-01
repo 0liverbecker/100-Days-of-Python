49958730d8a7656db9ef200bd7b54f294a3f894a
0liver <becker.teolb@gmail.com>
2021-11-01 17:33:19 +0100

    day Six part One is over

--- Days/Day_Six.py
+++ Days/Day_Six.py
@@ -2,3 +2,87 @@
 #   Code Blocks/ Functions and while Loops
 #   Final Project - Escaping the Maze
 ###################################################################
+#   I define a Function with "def" an the Name of the function
+#def my_function(): ## then all what follow is a part of this Function
+#    print("hello")
+#   after the coding Block i call the function with this:
+#my_function()
+
+#   at the reeborgs world challenge, my Code was simple:
+#def q():
+#    move()
+#    turn_left()
+#    move()
+#    turn_left()
+#    move()
+#    turn_left()
+#    move()
+#    
+#q()
+#   The next challenge was easy, but i think im make that complicated again
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+#
+#def sign():
+#    turn_left()    
+#    move()
+#    turn_right()
+#    move()
+#    turn_right()
+#    move()
+#    turn_right()
+#    move()
+#    turn_right()
+#    
+#sign()
+#   the next Challenge was pretty easy:
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+#    
+#def pattern():
+#    move()
+#    turn_left()
+#    move()
+#    turn_right()
+#    move()
+#    turn_right()
+#    move()
+#    turn_left()
+#    
+#def run():
+#    pattern()
+#    pattern()
+#    pattern()
+#    pattern()
+#    pattern()
+#    pattern()
+#    
+#run()
+#   i make on def for the missing turn_right, on for the pattern and the last on for the number of repeats
+#   but its easy to make it with a for loop
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+#    
+#def pattern():
+#    move()
+#    turn_left()
+#    move()
+#    turn_right()
+#    move()
+#    turn_right()
+#    move()
+#    turn_left()
+#    
+#for run in range(6):
+#    pattern()
+#   Now i have 8 Line of Code less.
+
+
+#############################################
+#   Indentation
