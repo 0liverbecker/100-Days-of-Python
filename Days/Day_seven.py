039314eae654963ed80827d793c0b8e861e8dbd2
0liver <becker.teolb@gmail.com>
2022-03-27 12:40:15 +0200

    ready

--- Days/Day_seven.py
+++ Days/Day_seven.py
@@ -169,7 +169,13 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
-
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+        
+        
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -179,3 +185,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
