3f0fc440f1c13a3e900b0b0c3cb87bc063b9f233
0liver <becker.teolb@gmail.com>
2022-04-01 19:34:59 +0200

    new day, new fun

---
+++ Days/Day_eight.py
@@ -0,0 +1,18 @@
+''' in this Lesson i write a program that Encrypt Data and encrypt this.
+
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+#def greet():
+#    print ("number one")
+#    print ("number two")
+#    print ("number three")
+#greet()
+# now we do the same but with a input
+#def greet_with(name):
+#    print (f"hey {name}")
+#
+#greet_with("oli")
+'''
+
