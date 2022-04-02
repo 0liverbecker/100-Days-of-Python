b587619d9c9b728ddcd9b645191ad93e857fff22
0liver <becker.teolb@gmail.com>
2022-04-02 21:27:53 +0200

    hard Bug Hunt for me ;)

--- Days/Day_eight.py
+++ Days/Day_eight.py
@@ -47,7 +47,7 @@ def primecheck():
     else:
         print("Goodbye")
 primecheck()
-'''
+
 # here is the right solution
 
 def prime_checker(number):
@@ -61,7 +61,42 @@ def prime_checker(number):
         print("Its not a prime number.")
 n = int(input("Check this number: "))
 prime_checker(number=n)
-  
+'''
+from base64 import encode
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+    ausgabetext = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        ausgabetext += new_letter
+    print (f"Der geheime Code lautet :{ausgabetext}")
         
+if direction == "decode":
+    encrypt(text, shift)
+else:
+    shift = (26-shift)
+    encrypt(text, shift)
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word         
     
 
